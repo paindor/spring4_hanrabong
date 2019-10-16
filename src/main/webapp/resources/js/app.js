6eb77617350269483743b1105554a591975a9859
paindor <paindorrr@gmail.com>
2019-10-16 18:34:27 +0900

    191016

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -1,11 +1,43 @@
-var app = (()=>{
-		return {
-			init : (ctx)=>{
-				service.join(ctx);
-				service.login(ctx);
-				service.go_join(ctx);
-			}
-		};
+"user strict";
+
+var app = app || {};
+
+app = (()=>{
+	const WHEN_ERR = '호출하는 js파일을 찾지 못했습니다';
+	let _, js ,authjs;
+	let run =x=>
+		$.getScript(x+'/resources/js/cmm/router.js', ()=>{
+			$.extend (new Session(x));
+			onCreate();
+			
+			
+			//alert(WHEN_ERR);
+			init();
+			
+		});
+	let init =()=>{
+			_=$.ctx();
+			js = $.js() ;
+			authjs = js+'/cmm/auth.js';
+		}
+	let onCreate=()=>{
+		init();
+		$.when(
+				$.getScript(authjs)
+		)
+		.done(()=>{
+			auth.onCreate()
+		}
+		
+		)
+		.fail(()=>{
+			alert(WHEN_ERR)
+		}
+				
+		)
+	}
+		
+		return {run:run}
 })();
 var user = (()=>{
 	var _empNo, _eName, _job, _mgr, _hireDate, _sal, _comm, _deptNo;
