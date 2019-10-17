f8bc3cfa5543e1e74d06614e60d96d48c12c31a1
paindor <paindorrr@gmail.com>
2019-10-17 19:23:02 +0900

    191017

--- src/main/java/com/hanrabong/web/controllers/HCustController.java
+++ src/main/java/com/hanrabong/web/controllers/HCustController.java
@@ -1,13 +1,21 @@
 package com.hanrabong.web.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.hanrabong.web.domains.HCustDTO;
 import com.hanrabong.web.seviceImpls.HCustServiceImpl;
 
 @Controller
@@ -17,13 +25,24 @@ public class HCustController {
 	private static final Logger logger = LoggerFactory.getLogger(HCustController.class);
 	@Autowired HCustServiceImpl empService;
 	
-	@GetMapping("/count")
-	public String count(Model model) {
+	
+	@PostMapping("/join")
+	public @ResponseBody Map<?,?> join(@RequestBody HCustDTO hcust) {
+		
+		//logger.info("ajax 가 보낸아이디 와 비번 {}" , hcust.getCid() + ","+ hcust.getCpw());
+		HashMap<String, String> map = new HashMap<>();
+		
+		map.put("cid",hcust.getCid() );
+		map.put("cpw",hcust.getCpw());
+		
+		
+		logger.info("map의 아이디 비번{}" , map.get("cid") + "," + map.get("cpw"));
+		
 		//int count = empService.countEmp();
 		
 		//model.addAttribute("count" , count);
 		
-		return "index";
+		return map;
 		
 		
 	}
--- src/main/java/com/hanrabong/web/domains/HCustDTO.java
+++ src/main/java/com/hanrabong/web/domains/HCustDTO.java
@@ -8,6 +8,106 @@ import lombok.Data;
 
 @Data @Component
 public class HCustDTO implements Serializable{
+	public String getCnum() {
+		return cnum;
+	}
+
+	public void setCnum(String cnum) {
+		this.cnum = cnum;
+	}
+
+	public String getCid() {
+		return cid;
+	}
+
+	public void setCid(String cid) {
+		this.cid = cid;
+	}
+
+	public String getCpw() {
+		return cpw;
+	}
+
+	public void setCpw(String cpw) {
+		this.cpw = cpw;
+	}
+
+	public String getCname() {
+		return cname;
+	}
+
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+
+	public String getGen() {
+		return gen;
+	}
+
+	public void setGen(String gen) {
+		this.gen = gen;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+	public String getSkinProb() {
+		return skinProb;
+	}
+
+	public void setSkinProb(String skinProb) {
+		this.skinProb = skinProb;
+	}
+
+	public String getSkinType() {
+		return skinType;
+	}
+
+	public void setSkinType(String skinType) {
+		this.skinType = skinType;
+	}
+
+	public String getChild() {
+		return child;
+	}
+
+	public void setChild(String child) {
+		this.child = child;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getCpoint() {
+		return cpoint;
+	}
+
+	public void setCpoint(String cpoint) {
+		this.cpoint = cpoint;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
 	private static final long serialVersionUID= 1L;
 
 	private String cnum, cid, cpw, cname, gen, birth, skinProb, skinType, child, addr, tel, cpoint;
